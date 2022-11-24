740d829c831a3f9a1657aa6342f2ba29519c19f7
cronon <dzmitry.avdeev@wolterskluwer.com>
2022-11-24 19:00:27 +0400

    upload videos with description and title

--- app/src/main/youtubeLogic.ts
+++ app/src/main/youtubeLogic.ts
@@ -47,44 +47,73 @@ async function youtubeUpload({mp4Path, title, description}: {mp4Path: string, ti
   if (!client || !isLoggedIn) {
     return {url: '', err: 'You\'re not logged in to Youtube'}
   }
+  const youtube = await getYoutube();
   if (!fs.existsSync(mp4Path)) {
     return {url: '', err: `Cannot find file ${mp4Path}`}
   }
   const fileSize = fs.statSync(mp4Path).size;
-  // const res = await youtube.videos.insert(
-  //   {
-  //     part: 'id,snippet,status',
-  //     notifySubscribers: false,
-  //     requestBody: {
-  //       snippet: {
-  //         title: 'Node.js YouTube Upload Test',
-  //         description: 'Testing YouTube upload via Google APIs Node.js Client',
-  //       },
-  //       status: {
-  //         privacyStatus: 'private',
-  //       },
-  //     },
-  //     media: {
-  //       body: fs.createReadStream(fileName),
-  //     },
-  //   },
-  //   {
-  //     // Use the `onUploadProgress` event from Axios to track the
-  //     // number of bytes uploaded to this point.
-  //     onUploadProgress: evt => {
-  //       const progress = (evt.bytesRead / fileSize) * 100;
-  //       readline.clearLine(process.stdout, 0);
-  //       readline.cursorTo(process.stdout, 0, null);
-  //       process.stdout.write(`${Math.round(progress)}% complete`);
-  //     },
-  //   }
-  // );
 
-  // console.log(res.data);
+  const res = await youtube.videos.insert({
+    part: ['id','snippet','status'],
+    notifySubscribers: false,
+    requestBody: {
+      snippet: {
+        title: title,
+        description: description,
+      },
+      status: {
+        privacyStatus: 'private',
+      },
+    },
+    media: {
+      body: fs.createReadStream(mp4Path),
+    }
+  },
+  {onUploadProgress: e => console.log('UPLOADPROGERSS', e.bytesRead)}
+  );
+
+    // {
+    //   // Use the `onUploadProgress` event from Axios to track the
+    //   // number of bytes uploaded to this point.
+    //   onUploadProgress: evt => {
+    //     const progress = (evt.bytesRead / fileSize) * 100;
+    //     readline.clearLine(process.stdout, 0);
+    //     readline.cursorTo(process.stdout, 0, null);
+    //     process.stdout.write(`${Math.round(progress)}% complete`);
+    //   },
+    // }
+
+  console.log(res.data);
   return {url: 'youtube://123.flv'+fileSize+mp4Path, err: null}
 }
-
-
-
-
+// res.data
+// {
+//   kind: 'youtube#video',
+//   etag: 'fXCTK2S5icyg7LCfXXvqEVIe8SU',
+//   id: 'KH_fV2RWYnI',
+//   snippet: {
+//     publishedAt: '2022-11-24T14:56:39Z',
+//     channelId: 'UC8TQpd6N4CMKXi7qNNRfduA',
+//     title: 'Camellia (Feat. Nanahira) - ÒâÖÒâ╝Òé╣Òâ®ÒéñÒâ│ÒéäÒüúÒüªÒéï´╝ƒþ¼æ (Can I Friend You On Bassbook Lol)',
+//     description: 'Kiara - Camellia (Feat. Nanahira) - ÒâÖÒâ╝Òé╣Òâ®ÒéñÒâ│ÒéäÒüúÒüªÒéï´╝ƒþ¼æ (Can I Friend You On Bassbook Lol)\n' +
+//       'Mustard seed (2021)\n' +
+//       'https://soundcloud.com/kiarabirth',
+//     thumbnails: { default: [Object], medium: [Object], high: [Object] },
+//     channelTitle: 'Cronon11',
+//     categoryId: '10',
+//     liveBroadcastContent: 'none',
+//     localized: {
+//       title: 'Camellia (Feat. Nanahira) - ÒâÖÒâ╝Òé╣Òâ®ÒéñÒâ│ÒéäÒüúÒüªÒéï´╝ƒþ¼æ (Can I Friend You On Bassbook Lol)',
+//       description: 'Kiara - Camellia (Feat. Nanahira) - ÒâÖÒâ╝Òé╣Òâ®ÒéñÒâ│ÒéäÒüúÒüªÒéï´╝ƒþ¼æ (Can I Friend You On Bassbook Lol)\n' +
+//         'Mustard seed (2021)\n' +
+//         'https://soundcloud.com/kiarabirth'
+//     }
+//   },
+//   status: {
+//     uploadStatus: 'uploaded',
+//     privacyStatus: 'private',
+//     license: 'youtube',
+//     embeddable: true,
+//     publicStatsViewable: true
+//   }
 // }
